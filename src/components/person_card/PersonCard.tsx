1d455fdb9e7b3ba479552e3457b25fffd2664acc
Piotr Bablok <bablok.piotr@gmail.com>
2021-12-03 19:22:56 +0100

    Create PersonCard component

---
+++ src/components/person_card/PersonCard.tsx
@@ -0,0 +1,12 @@
+import './PersonCard.css'
+
+
+const PersonCard: React.FC = () => {
+    return (
+        <div className='control-panel'>
+          
+        </div>
+    )
+}
+
+export default PersonCard;
